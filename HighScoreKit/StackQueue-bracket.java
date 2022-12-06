f391a8c4b5a35b3c237d58ff9ae04ef9c8851ce8
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-12-07 00:26:21 +0900

    bracket

---
+++ HighScoreKit/StackQueue-bracket.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+        }
+        
+        if(stack.isEmpty()){
+            answer=true;
+        }
+        else{
+            answer=false;
+        }
+        return answer;
+    }
+}
