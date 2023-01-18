dabe15dbccd0292e2a2bb4a999a05d1d120961d4
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-01-18 17:04:57 +0900

    twodivide

---
+++ Introduction100/twodivide.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double a = num1;
+        double b = num2;
+        double result = a / b * 1000;
+        int answer = (int)result;
+        return answer;
+    }
+}
