60ca76642ee7f78c084656cb6cde7ffa7a8f4337
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-01-25 14:26:49 +0900

    removechar

---
+++ Introduction100/removechar.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if(c==letter.charAt(0)){
+                answer+="";
+            }
+            else{
+                answer+=c;
+            }
+        }
+        return answer;
+    }
+}
