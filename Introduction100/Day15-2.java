b3135b2b82abaab19d40f274028375bbabdfbc66
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-01-27 17:56:03 +0900

    Day15-2

---
+++ Introduction100/Day15-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int j=0; j<list.size(); j++){
+            answer[j] = list.get(j);
+        }
+        return answer;
+    }
+}
