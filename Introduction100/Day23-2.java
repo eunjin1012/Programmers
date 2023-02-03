a5d223b81b9325e8f118b69ee203a15c18b5b405
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-02-03 11:23:00 +0900

    Day23-2

---
+++ Introduction100/Day23-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] word = {"aya", "ye", "woo", "ma"};
+        for(int i=0; i<babbling.length; i++){
+            for(int j=0; j<word.length; j++){
+                while(babbling[i].contains(word[j])){
+                    babbling[i] = babbling[i].replace(word[j], " ");
+                    if(babbling[i].trim().length()==0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
