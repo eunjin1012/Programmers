fad98d5c99443bde530153f90cb70b62d8b4719a
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-04-25 15:31:50 +0900

    대문자로 바꾸기

---
+++ BasicTraining/Uppercase.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        myString = myString.toUpperCase();
+        answer = myString;
+        return answer;
+    }
+}
