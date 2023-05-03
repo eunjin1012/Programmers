c4c5f73ee0b2af109a4a9da32f3fbb8a0fa488ca
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2023-05-03 10:11:03 +0900

    문자열 바꿔서 찾기

---
+++ BasicTraining/changeandfind.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String str = "";
+        for(int i=0; i<myString.length(); i++){
+            char ch = myString.charAt(i);
+            if(ch=='A'){
+                str += 'B';
+            }
+            else{
+                str += 'A';
+            }
+        }
+        if(str.contains(pat)){
+            answer = 1;
+        }
+        return answer;
+    }
+}
